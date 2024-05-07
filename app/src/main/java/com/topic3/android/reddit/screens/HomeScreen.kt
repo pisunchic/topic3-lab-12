fe9bfe59541f994896bebacce5926d070596cebb
pisunchic <120672338+pisunchic@users.noreply.github.com>
2024-05-07 18:21:02 +0300

    end HomeScreen

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -97,6 +97,42 @@ fun HomeScreen(viewModel: MainViewModel) {
                 Spacer(modifier = Modifier.height(6.dp))
             }
         }
+        val homeScreenItems = mapHomeScreenItems(posts)
+        Box(modifier = Modifier.fillMaxSize()) {
+            LazyColumn(
+                modifier = Modifier
+                    .background(color=MaterialTheme.colors.secondary),
+                content = {
+                    items(
+                        items = homeScreenItems,
+                        itemContent = {item ->
+                            if (item.type == HomeScreenItemType.TRENDING) {
+                                TrendingTopics(
+                                    trendingTopics = trendingItems,
+                                    modifier = Modifier.padding(
+                                        top = 16.dp, bottom = 6.dp
+                                    )
+                                )
+                            } else if (item.post !=null){
+                                val post = item.post
+                                if (post.type == PostType.TEXT) {
+                                    TextPost(
+                                        post = post,
+                                        onJoinButtonClick = onJoinClickAction
+                                    )
+                                } else {
+                                    ImagePost(
+                                        post = post,
+                                        onJoinButtonClick = onJoinClickAction
+                                    )
+                                }
+                                Spacer(modifier = Modifier.height(6.dp))
+                            }
+                        }
+                    )
+                }
+            )
+        }
         Box(
             modifier = Modifier
                 .align(Alignment.BottomCenter)
@@ -107,7 +143,20 @@ fun HomeScreen(viewModel: MainViewModel) {
     }
 
 }
-
+private fun mapHomeScreenItems(
+    posts: List<PostModel>
+) : List<HomeScreenItem> {
+    val homeScreenItems = mutableListOf<HomeScreenItem>()
+    homeScreenItems.add(
+        HomeScreenItem(HomeScreenItemType.TRENDING)
+    )
+    posts.forEach { post ->
+        homeScreenItems.add(
+            HomeScreenItem(HomeScreenItemType.POST, post)
+        )
+    }
+    return homeScreenItems
+}
 @Composable
 private fun TrendingTopics(
     trendingTopics: List<TrendingTopicModel>,
